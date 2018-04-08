bfc94476f7dfcfd2a5806ee56bbe3d0e6545f999
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-04-08 05:02:37 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -7,14 +7,14 @@ var index = require("../index.js")
 //       expect(index.name).toEqual('Susan')
 //     })
 //   })
-// 
-// 
+//
+//
 //   describe('Height', () => {
 //     it('is less than 40', () => {
 //       expect(index.height).toBeLessThan(40)
 //     })
 //   })
-// 
+//
 //   describe('Message', () => {
 //     it('gives the name and height', () => {
 //       expect(index.message).toInclude(index.name)
